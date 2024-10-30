513e59ee4068714058292fcb6eef206c7c025b96
alexyzha <alexyzha@gmail.com>
2024-10-30 15:57:00 -0700

    add

--- src/network.asm
+++ src/network.asm
@@ -1,69 +1,47 @@
 section .data
-    hw db 'Hello, World!', 0xA, 0
-    buffer db '00000000000000000000', 0xA, 0
-    EULER_CONST dq 2.718281828459045
-section .text
-    global _start                   ; define main entry point
-    fld qword [EULER_CONST]
-    fstp st0
-_start:
-    call print_string
+    EULER       dq  0x4005BF0A8B145769
+    FILENAME    db  'file.txt', 0
+    READONLY    equ 0
+    WRITEONLY   equ 1
+    READWRITE   equ 2
+    INTBUFFER   db '00000', 0xA, 0
 
-    mov rdi, 100
-    mov rsi, buffer
-    call print_int
-
-    mov rax, 60                     ; syscall exit
-    xor rdi, rdi                    ; return 0
-    syscall                         ; make exit syscall
-
-print_string:
-    mov rax, 1                      ; syscall write
-    mov rdi, 1                      ; file desc = stdout, rdi passed to caller
-    mov rsi, hw                     ; p->string
-    mov rdx, 14                     ; # bytes to write
-    syscall                         ; make write syscall
-    ret
-
-print_int:                          ; rdx contains [div rbx], rdx = rax % rbx, rax = rax / rbx
-    mov rax, rdi                    ; numerator
-    mov rbx, 10                     ; denominator
-    lea rcx, [rsi+19]               ; p->str in rsi, num in rdi, assumes rsi = buffer
-
-    pint_loop_start:
-        cmp rax, 0                  ; end loop if no digits left
-        je pint_loop_end
-        xor rdx, rdx
-        div rbx
-        add dl, '0'
-        mov [rcx], dl
-        dec rcx
-        jmp pint_loop_start
-
-    pint_loop_end:
-    ; rcx holds int (buffer)
-    ; need string len
-
-    inc rcx
-    mov rdi, rcx
-    call strlen
-    ; len in rax
+section .bss
     
-    mov rdx, rax
-    mov rsi, rcx
-    mov rdi, 1
-    mov rax, 1
+section .text
+    global _start
+
+_start:
+    ; just shit out what is in a file
+    mov rax, 2
+    mov rdi, FILENAME
+    mov rsi, READWRITE
+    syscall 
+
+    ; file handle in rax
+    ; read from file into buffer
+    mov rdi, rax
+    mov rax, 0
+    mov rsi, INTBUFFER
+    mov rdx, 5
     syscall
-    ret
 
-strlen:                             ; assumes there will be a null terminator
-    xor rax, rax
-    strlen_loop_start:
-        cmp byte [rdi], 0
-        je strlen_loop_end
-        inc rax
-        inc rdi
-        jmp strlen_loop_start
+    ; write to console
+    mov rax, 1
+    push rdi
+    mov rdi, 1
+    mov rsi, INTBUFFER
+    mov rdx, 7
+    syscall
 
-    strlen_loop_end:
-    ret
+    ; write back into file
+    mov rax, 1
+    pop rdi
+    mov rsi, INTBUFFER
+    mov rdx, 5
+    syscall
+
+    ; return 0
+    mov rax, 60             ; return 0, 60 to exit
+    mov rdi, 0
+    syscall
